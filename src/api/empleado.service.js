bfdab96ab45b145f3b2766c638fc76d7279e94ae
Facundo Diaz <dfacundo905@gmail.com>
2026-02-04 23:30:44 -0300

    se creo formulario para post empleados

--- src/api/empleado.service.js
+++ src/api/empleado.service.js
@@ -1,3 +1,4 @@
 import clientAxios from "./clientAxios.js";
 
 export const getEmpleados = () => clientAxios.get("/empleados");
+export const postEmpleados = (data) => clientAxios.post("/empleados/registro", data);
